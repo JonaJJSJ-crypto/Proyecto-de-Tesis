93c7a12d9ad5d19c5f1f0f6a4961d33e3f613d9b
JonaJJSJ-crypto <78813250+JonaJJSJ-crypto@users.noreply.github.com>
2021-06-24 18:46:23 -0500

    UpdatedLWSM Feynrules sim

---
+++ Simulacion/LWSM/LWSM200AutoD/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.47
+# Mathematica version: 12.1.1 for Microsoft Windows (64-bit) (June 19, 2020)
+# Date: Wed 23 Jun 2021 21:57:59
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ Simulacion/LWSM/LWSM200AutoND/coupling_orders.py
@@ -0,0 +1,16 @@
+# This file was automatically created by FeynRules 2.3.47
+# Mathematica version: 12.1.1 for Microsoft Windows (64-bit) (June 19, 2020)
+# Date: Wed 23 Jun 2021 21:58:40
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
