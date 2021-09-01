edbc5fd88fa7c9c041cbe236f7fb33bbd10b4af7
JonaJJSJ-crypto <78813250+JonaJJSJ-crypto@users.noreply.github.com>
2021-09-01 11:09:46 -0500

    updated FeynRules Folder

---
+++ Simulacion/FeynRules/LWSM200/LWSM200DnR_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.47
+# Mathematica version: 12.1.1 for Microsoft Windows (64-bit) (June 19, 2020)
+# Date: Wed 1 Sep 2021 10:52:45
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
